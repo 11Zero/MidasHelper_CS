72c3d7ee09c87adf4c195307d622a5a9e2d46d7b
11Zero <874612467@qq.com>
2017-03-07 00:21:41 +0800

    添加openGL中，画出第一条线，UI布局完成

--- MidasHelper_CS/SectionForm.xaml.cs
+++ MidasHelper_CS/SectionForm.xaml.cs
@@ -23,7 +23,13 @@ namespace MidasHelper_CS
         /// The current rotation.
         /// </summary>
         private float rotation = 0.0f;
-
+        private int xLength = 0;///x方向实际总长
+        private int yLength = 0;///y方向实际总长
+        private double xTimes = 1.0;///x方向缩放倍数
+        private double yTimes = 1.0;///y方向缩放倍数
+        private double xRange = 0;///x方向图形尺寸
+        private double yRange = 0;///y方向图形尺寸
+        //private OpenGL gl = null;
         public SectionForm()
         {
             InitializeComponent();
@@ -31,7 +37,9 @@ namespace MidasHelper_CS
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
-
+            //gl = openGLControl.OpenGL;
+            //gl.Clear(OpenGL.GL_COLOR_BUFFER_BIT | OpenGL.GL_DEPTH_BUFFER_BIT);
+            //gl.LoadIdentity();
         }
         /// <summary>
         /// Handles the OpenGLDraw event of the openGLControl1 control.
@@ -40,10 +48,10 @@ namespace MidasHelper_CS
         /// <param name="args">The <see cref="SharpGL.SceneGraph.OpenGLEventArgs"/> instance containing the event data.</param>
         private void openGLControl_OpenGLDraw(object sender, OpenGLEventArgs args)
         {
-            return;
             //  Get the OpenGL object.
+            return;
             OpenGL gl = openGLControl.OpenGL;
-
+            gl = openGLControl.OpenGL;
             //  Clear the color and depth buffer.
             gl.Clear(OpenGL.GL_COLOR_BUFFER_BIT | OpenGL.GL_DEPTH_BUFFER_BIT);
 
@@ -85,6 +93,35 @@ namespace MidasHelper_CS
             rotation += 3.0f;
         }
 
+        ///// <summary>
+        ///// 
+        ///// </summary>
+        ///// <param name="nX"></param>
+        ///// <param name="nY"></param>
+        ///// <param name="length">线长度</param>
+        ///// <param name="col"></param>
+        ///// <param name="isArrow"></param>
+        ///// <param name="withBorder"></param>
+        /// <summary>
+        /// 画直线
+        /// </summary>
+        /// <param name="sX">起点x坐标</param>
+        /// <param name="sY">起点y坐标</param>
+        /// <param name="eX">终点x坐标</param>
+        /// <param name="eY">终点y坐标</param>
+        /// <param name="isArrow">是否带箭头</param>
+        /// <param name="withBorder">是否带边界线</param>
+        private void drawLine(int sX, int sY, int eX, int eY,bool isArrow=false, bool withBorder=true)
+        {
+            OpenGL gl = openGLControl.OpenGL;
+            gl.Clear(OpenGL.GL_COLOR_BUFFER_BIT | OpenGL.GL_DEPTH_BUFFER_BIT);
+            gl.LoadIdentity();
+            gl.Color(1.0f, 0.0f, 0.0f);
+            gl.Begin(SharpGL.Enumerations.BeginMode.Lines);
+            gl.Vertex(sX * xRange, sY * yRange);
+            gl.Vertex(eX * xRange, eY * yRange);
+            gl.End();
+        }
         /// <summary>
         /// Handles the OpenGLInitialized event of the openGLControl1 control.
         /// </summary>
@@ -96,9 +133,15 @@ namespace MidasHelper_CS
 
             //  Get the OpenGL object.
             OpenGL gl = openGLControl.OpenGL;
-
+            xTimes = 0.8;
+            yTimes = 0.8;
+            xLength = 2;
+            yLength = 2;
+            xRange = xTimes/xLength;///opengl整个空间大小为-1，-1到1,1，所以画线时需根据实际尺寸及绘图空间进行缩放
+            yRange = yTimes/yLength;
             //  Set the clear color.
             gl.ClearColor(0, 0, 0, 0);
+            //gl.Translate(-xTimes / 5, yTimes / 5, 0);
         }
 
         /// <summary>
@@ -108,6 +151,7 @@ namespace MidasHelper_CS
         /// <param name="args">The <see cref="SharpGL.SceneGraph.OpenGLEventArgs"/> instance containing the event data.</param>
         private void openGLControl_Resized(object sender, OpenGLEventArgs args)
         {
+            return;
             //  TODO: Set the projection matrix here.
 
             //  Get the OpenGL object.
@@ -147,7 +191,14 @@ namespace MidasHelper_CS
 
         private void btn_test_Click(object sender, RoutedEventArgs e)
         {
-            drawOneSection();
+            drawLine(0,0,1,1);
+            //drawLine(0, 0, -2, 2);
+            //drawOneSection();
+        }
+
+        private void text_h01_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
         }
 
     }
